151997d40b791645ece0d06d297c4c6baf00f419
ilias-zet <iliaszerman@gmail.com>
2022-03-25 23:57:57 +0300

    F-function & S-blocks started

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ const pc2 = [
 ];
 
 // Таблица расширения Е
-const e = [
+const eTable = [
     31,0,1,2,3,4,3,4,
     5,6,7,8,7,8,9,10,
     11,12,11,12,13,14,15,16,
@@ -80,8 +80,58 @@ const e = [
     27,28,27,28,29,30,31,0
 ];
 
+const sTable = [
+    [
+        [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
+        [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
+        [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
+        [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]
+    ],
+    [
+        [15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10],
+        [3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],
+        [0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],
+        [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9]
+    ],
+    [
+        [10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
+        [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
+        [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
+        [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12]
+    ],
+    [
+        [7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
+        [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
+        [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
+        [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14]
+    ],
+    [
+        [2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
+        [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
+        [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
+        [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3]
+    ],
+    [
+        [12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
+        [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
+        [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
+        [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13]
+    ],
+    [
+        [4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
+        [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
+        [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
+        [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12]
+    ],
+    [
+        [13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
+        [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
+        [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
+        [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]
+    ]
+]
 // Массив значений битовых сдвигов ключа
-const byteMoves = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1];
+const biteMoves = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1];
 
 const encrypt = (message, key) => {
     // Заменяем каждую букву на соответствующее ей битовое значение из объекта
@@ -91,8 +141,8 @@ const encrypt = (message, key) => {
     print('Бинарный ключ:', binaryKey);
 
     // Дополняем ключ и сообщение до 64-битного формата
-    binaryMessage = get64bytes(binaryMessage);
-    binaryKey = get64bytes(binaryKey);
+    binaryMessage = get64bites(binaryMessage);
+    binaryKey = get64bites(binaryKey);
     print('64-битное сообщение:', binaryMessage);
     print('64-битный ключ:', binaryKey);
     
@@ -129,10 +179,22 @@ const encrypt = (message, key) => {
 
     // Формируем массив объединённых частей ключей
     print('Формируем массив объединённых частей ключей');
-    const keys = leftKeysArr.map((leftKey, idx) => leftKey + rightKeysArr[idx]);
+    let keys = leftKeysArr.map((leftKey, idx) => leftKey + rightKeysArr[idx]);
     keys.forEach((key, idx) => {
         print(`<div style='font-size:10px'>${idx+1} key: ${key}</div>`);
     })
+
+    // Производим сжатие данных ключей
+    print('Производим сжатие данных ключей до 48 бит');
+    keys = keys.map(key => get48key(key));
+    keys.forEach((key, idx) => {
+        print(`<div style='font-size:10px'>${idx+1} key: ${key}</div>`);
+    })
+
+    F(keys[0], msgRight);
+    // keys.forEach(key => {
+    //     F(key, msgRight);
+    // })
 }
 
 const get16keys = (keyLeft, keyRight, mode) => {
@@ -141,7 +203,7 @@ const get16keys = (keyLeft, keyRight, mode) => {
     let direction;
     if (mode === 'initial') direction = 'left';
     else if (mode === 'final') direction = 'right';
-    byteMoves.forEach(bitesCount => {
+    biteMoves.forEach(bitesCount => {
         keyLeft = bitShift(keyLeft, direction, bitesCount);
         keyRight = bitShift(keyRight, direction, bitesCount);
         leftKeysArr.push(keyLeft);
@@ -151,18 +213,18 @@ const get16keys = (keyLeft, keyRight, mode) => {
 }
 
 // Функция дополняет входящую строку до числа бит кратному 64
-const get64bytes = (str) => {
+const get64bites = (str) => {
     while(str.length % 64 !== 0) str += '0';
     return str;
 }
 
 // Функция делает циклический битовый сдвиг
-const bitShift = (bytesStr, direction, count) => {
-    let bytesArr = bytesStr.split('');
+const bitShift = (bitesStr, direction, count) => {
+    let bitesArr = bitesStr.split('');
     let k;
     if (direction == 'right') k = -1;
     else if (direction == 'left') k = 1;
-    return bytesArr.splice(k * count).concat(bytesArr).join('');
+    return bitesArr.splice(k * count).concat(bitesArr).join('');
 }
 
 
@@ -170,11 +232,11 @@ const bitShift = (bytesStr, direction, count) => {
 const permutation = (mode, message) => {
     let arr = [];
     let permArr = [];
-    const msgBytesArr = message.split('');
+    const msgBitesArr = message.split('');
     if (mode == 'initial') permArr = ip;
     else if (mode == 'final') permArr = ip_1;
     for (let i = 0; i < ip.length; i++) {
-        arr[i] = msgBytesArr[permArr[i]];
+        arr[i] = msgBitesArr[permArr[i]];
     }
     return arr.join('');
 }
@@ -182,14 +244,14 @@ const permutation = (mode, message) => {
 // Функция делает перестановку бит в переданном в нее ключе согласно таблице PC-1
 const get56key = (key) => {
     let arr = [];
-    const keyBytesArr = key.split(''); 
+    const keyBitesArr = key.split(''); 
     for(let i = 0; i < key.length; i++) {
-        arr[i] = keyBytesArr[pc1[i]];
+        arr[i] = keyBitesArr[pc1[i]];
     }
     return arr.join('');
 }
 
-// функция делает перестановку бит согласно таблице PC-2
+// Функция делает перестановку бит в переданном в нее ключе согласно таблице PC-2
 const get48key = (key) => {
     let arr = [];
     const keyBitesArr = key.split('');
@@ -199,7 +261,81 @@ const get48key = (key) => {
     return arr.join('');
 }
 
+// Функция расширение Е
+const E = (msgPart) => {
+    let arr = [];
+    const msgBitesArr = msgPart.split('');
+    for(num of eTable) {
+        arr.push(msgBitesArr[num]);
+    }
+    return arr.join('');
+}
 
+// Основная функция шифрования
+const F = (keyN, msgPart) => {
+    print('<b>Функция F</b>')
+    print('Входящее сообщение:', msgPart);
+    print('Ключ:', keyN);
+
+    // Расширяем количество бит входящего сообщения до 48 через функцию Е
+    msgPart = E(msgPart);
+    print('Расширенное сообщение:', msgPart);
+
+    // Складываем расширенное сообщение по модулю 2 с ключом
+    const xored = XOR(msgPart, keyN);
+    print('Результат XOR', xored);
+
+    // Получаем B блоки
+    const Bblocks = getB6blocks(xored);
+    print('<b>B-блоки (6 бит):', Bblocks.join(', '), '</b>');
+
+    // Получаем В` блоки
+    const B4blocks = Bblocks.map((Bblock, idx) => getB4block(Bblock, idx));
+    print(`<b>B'-блоки (4 бит):`, B4blocks.join(', '), '</b>');
+}
+
+const getB4block = (B6block, blockIdx) => {
+    print(`<b>Блок ${B6block}</b>`);
+
+    // Получаем номер строки таблицы
+    const binaryRow = B6block[0] + B6block[5];
+    const row = parseInt(binaryRow, 2);
+    print(`Крайние биты блока ${binaryRow}, значит номер строки таблицы ${row}`);
+
+    // Получаем номер столбца таблицы
+    const binaryColumn = B6block.slice(1,5);
+    const column = parseInt(binaryColumn, 2);
+    print(`Центральные биты блока ${binaryColumn}, значит номер столбца таблицы ${column}`);
+
+    // Получаем значение из таблицы
+    const table = sTable[blockIdx];
+    const numeric = table[row][column];
+
+    // Получаем битовое значение результата из таблицы
+    let B4block = numeric.toString(2);
+    while (B4block.length < 4) B4block = '0' + B4block;
+    print(`Числовое значение из таблицы ${numeric}, что эквивалентно ${B4block} в бинарном виде`);
+    return B4block;
+}
+
+// Исключающее ИЛИ
+const XOR = (bytes1, bytes2) => {
+    const bytesArr1 = bytes1.split('');
+    const bytesArr2 = bytes2.split('');
+    return bytesArr1.map((el, idx) => el ^ bytesArr2[idx]).join('');
+}
+
+const getB6blocks = (bites) => {
+    const arr = [];
+    let start = 0;
+    let finish = 6;
+    while (finish <= bites.length) {
+        arr.push(bites.slice(start,finish));
+        start = finish;
+        finish += 6;
+    }
+    return arr;
+}
 
 encrypt(message, key);
 // 
